53ac86103813b05b210b6c900ead7e5b2f5fd518
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-31 17:51:56 +0200

    add scaffolding and prepared data

---
+++ films-data.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Films data
+ */
+
+$films = [
+    [
+        'title' => 'Harry Potter e la pietra filosofale',
+        'description' => 'Harry Potter è un ragazzo orfano allevato da sua zia e suo zio. All\'età di undici anni, il mezzogigante Rubeus Hagrid lo informa che è in realtà un mago e che i suoi genitori sono stati uccisi da un malvagio e temuto mago di nome Lord Voldemort. Voldemort ha anche tentato di uccidere il piccolo Harry nella stessa notte, ma la sua maledizione è misteriosamente rimbalzata riducendolo a una forma debole e indifesa. Per questo Harry è diventato molto famoso nel mondo magico. Harry inizia il suo primo anno alla Scuola di Magia e Stregoneria di Hogwarts e impara a conoscere la magia. Durante l\'anno, Harry e i suoi amici Ron Weasley e Hermione Granger si interessano alla pietra filosofale, nascosta all\'interno della scuola.',
+        'year' => 2001,
+    ],
+    [
+        'title' => 'Harry Potter e la camera dei segreti',
+        'description' => 'Harry, Ron ed Hermione tornano a Hogwarts per il loro secondo anno, che dimostra di essere più impegnativo di quello precedente. La Camera dei Segreti è stata aperta, lasciando alcuni studenti e fantasmi pietrificati dal mostro nascosto al suo interno. Harry deve affrontare il rischio di essere l\'erede di Salazar Serpeverde (fondatore della camera), scopre di potere parlare il Serpentese, e trova un misterioso diario legato alla Camera dei Segreti appartenuto allo studente che, in futuro, cambierà nome e diventerà tristemente noto.',
+        'year' => 2002,
+    ],
+    [
+        'title' => 'Harry Potter e il prigioniero di Azkaban',
+        'description' => 'Il terzo anno a Hogwarts vede il maghetto, insieme ai suoi amici, frequentare ancora la Scuola di Hogwarts. Il Professor R.J. Lupin si unisce al personale come insegnante di Difesa Contro le Arti Oscure, mentre il condannato assassino Sirius Black scappa dalla prigione di Azkaban. Il Ministero della Magia manda dei malvagi Dissennatori di Azkaban a sorvegliare Hogwarts per proteggerla da Black. Harry viene a sapere di più sul suo passato e la sua connessione con il prigioniero evaso.',
+        'year' => 2004,
+    ],
+    [
+        'title' => 'Harry Potter e il calice di fuoco',
+        'description' => 'Durante il quarto anno di Harry, il Marchio Nero appare nel cielo dopo un attacco dei Mangiamorte alla Coppa del Mondo di Quidditch, Hogwarts ospita un evento leggendario: il Torneo Tremaghi, c\'è un nuovo professore di Difesa Contro le Arti Oscure, Alastor Moody, e frequenti incubi assillano Harry tutto l\'anno. Tre scuole europee partecipano al torneo, con tre "campioni" che rappresentano ciascuna scuola in prove pericolose. Il Calice di fuoco sceglie Fleur Delacour, Viktor Krum e Cedric Diggory per competere uno contro l\'altro. Tuttavia, curiosamente, il nome di Harry esce dal Calice rendendo così lui il quarto campione del Torneo, che si concluderà in un incontro terrificante con un rinato Lord Voldemort.',
+        'year' => 2005,
+    ],
+    [
+        'title' => 'Harry Potter e l\'Ordine della Fenice',
+        'description' => 'Prima dell\'inizio dell\'anno, Harry viene attaccato dai Dissennatori a Little Whinging, provocando problemi con il ministero della magia. Più tardi, scopre che il Ministro nega il grande ritorno del terribile mago Lord Voldemort. Harry è anche afflitto da incubi inquietanti e realistici, mentre la Professoressa Umbridge, un rappresentante del Ministro della Magia Cornelius Caramell, è la nuova insegnante di Difesa Contro le Arti Oscure e prende molto presto il dominio della scuola. Harry si rende conto che Voldemort e lui sono legati grazie a una profezia che rivela "nessuno dei due può vivere se l\'altro sopravvive". Intanto numerosi eventi si susseguono: la ribellione che coinvolge gli studenti di Hogwarts; l\'organizzazione segreta dell\'Ordine della Fenice, lo strano comportamento del Ministero della Magia e i Mangiamorte che iniziano a radunarsi nuovamente.',
+        'year' => 2007,
+    ],
+    [
+        'title' => 'Harry Potter e il principe mezzosangue',
+        'description' => 'Nel sesto anno di Harry a Hogwarts, Lord Voldemort e i suoi Mangiamorte stanno facendo crescere il terrore in maghi e Babbani. Albus Silente convince il suo vecchio amico Horace Lumacorno a tornare a Hogwarts come insegnante per un posto vacante da ricoprire. C\'è una ragione più importante, tuttavia, per il ritorno di Lumacorno. A una lezione di Pozioni Harry entra in possesso di un libro di testo scolastico stranamente annotato, su cui è scritto "Questo libro è di proprietà del Principe Mezzosangue" che si rivela estremamente utile ai suoi studi. Draco Malfoy intanto si sforza di adempiere un atto a lui richiesto da Voldemort. Nel frattempo Silente e Harry segretamente lavorano insieme per scoprire il metodo con cui distruggere il Signore Oscuro una volta per tutte.',
+        'year' => 2009,
+    ],
+    [
+        'title' => 'Harry Potter e i Doni della Morte – Parte 1',
+        'description' => 'Dopo gli eventi imprevisti alla fine del precedente episodio a Harry, Ron e Hermione è affidata la missione di trovare e distruggere il segreto dell\'immortalità di Lord Voldemort - gli Horcrux. Dovrebbe essere il loro ultimo anno a Hogwarts, ma il crollo del Ministero della Magia e l\'ascesa di Voldemort al potere impediscono loro di frequentare le lezioni. Il trio affronta una lunga avventura con molti ostacoli tra Mangiamorte e Ghermidori. Nella ricerca dei misteriosi Doni della Morte il collegamento di Harry con la mente del Signore Oscuro diventa sempre più forte.',
+        'year' => 2010,
+    ],
+    [
+        'title' => 'Harry Potter e i Doni della Morte – Parte 2',
+        'description' => 'Dopo avere distrutto un Horcrux e scoperto il significato dei tre Doni della Morte Harry, Ron e Hermione continuano a cercare gli altri Horcrux, nel tentativo di distruggere Voldemort, che ora ha ottenuto la Bacchetta di Sambuco. Il Signore Oscuro scopre la ricerca che Harry e i suoi amici hanno iniziato e lancia un attacco alla scuola di Hogwarts, dove il trio, gli allievi e tutti gli insegnanti tentano un\'ultima resistenza contro le forze oscure.',
+        'year' => 2011,
+    ],
+];
